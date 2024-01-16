ff9ac23e383d23d3bfa4d4073b36c61cb3bc0b36
Hyeryeon-Seo <121484282+Hyeryeon-Seo@users.noreply.github.com>
2024-01-16 09:50:42 +0900

    [level Lv.1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/Lv.1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -4,7 +4,6 @@ function solution(n) {
             // x=1부터 for문을 돌리며 확인하므로, 가장 작은 x가 먼저 if문 조건에 걸리게 되어있다
             // 바로 x를 답으로 리턴
             return x; 
-            break; // 최소 자연수만 리턴하고 멈추기
         }
     }
 }
