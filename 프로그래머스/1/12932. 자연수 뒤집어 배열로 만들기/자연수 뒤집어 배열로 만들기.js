a53d1772c5fd52818abfdfad515fc8366311e84c
Hyeryeon-Seo <121484282+Hyeryeon-Seo@users.noreply.github.com>
2024-01-24 10:03:34 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    const str = String(n); // 숫자 -> 문자열
-    const mapfn = (arg) => Number(arg) // element를 문자열->정수로 바꿔주는 mapfn 정의
-    const preArr = Array.from(str, mapfn); // 문자열 -> 배열(정수가 원소인)
-    const newArr = [];
-    for(let i = preArr.length-1; i >= 0; i--) {
-        newArr.push(preArr[i]);
-    }
-     return newArr;
+function solution(n) {
+    
+    const str = String(n);
+    var answer = Array.from(str);
+    return answer;
+}
+
+console.log(solution(12345));
 }
