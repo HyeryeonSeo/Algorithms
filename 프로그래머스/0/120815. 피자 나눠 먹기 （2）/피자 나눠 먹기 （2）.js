065bd2a7be1627727722fba29437169b3fb95c1b
Hye-ryeon <121484282+Hyeryeon-Seo@users.noreply.github.com>
2024-08-12 21:37:15 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,12 +1,12 @@
 function solution(n) {
-    let pizNum = 6;
+    let piece = 6;
     
-    while (pizNum <= 6*n) { // 이 조건이 맞는 한 계속 반복
-        if (pizNum % 6 === 0 && pizNum % n === 0) {
+    while (piece <= 6*n) { 
+        if (piece % n === 0) {
             break;
         }
-        pizNum++;
+        piece += 6;
     }
     
-    return pizNum/6;
+    return piece / 6;
 }
