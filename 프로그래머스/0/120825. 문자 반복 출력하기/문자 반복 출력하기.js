ba1eaf6683c1985ebe949c4005eca360b49f4cb8
Hye-ryeon <121484282+Hyeryeon-Seo@users.noreply.github.com>
2024-08-31 20:45:38 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -4,7 +4,5 @@ function solution(my_string, n) {
         let repeated = c.repeat(n);
         repeatedArray.push(repeated);
     })
-    // return toString(repeatedArray); 이렇게 쓰면안됨 그냥 join() 안됨.
-    // join 안쓰면 hhh,eee, .. 콤마찍힘
     return repeatedArray.join("").toString();
 }
