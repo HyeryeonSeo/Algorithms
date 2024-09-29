18540d85a9223f92f0ba4164ebad0f9fd038907b
Ryeon Seo <121484282+HyeryeonSeo@users.noreply.github.com>
2024-09-30 00:00:50 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,6 +1,5 @@
 function solution(n, numlist) {
-    // filter 는 새 배열을 리턴 , splice 는 인덱스로 제거
-    return numlist.filter(e => {
-        return e%n == 0
-    })
+    // filter 는 새 배열을 리턴 (안 콜백함수, 한 줄이면 {return..}필요x)
+    // + splice 는 인덱스로 제거할때
+    return numlist.filter(num => num % n === 0);
 }
