0c81a7b2a221231b622f8c17fafe988aa8a6eee0
Ryeon Seo <121484282+HyeryeonSeo@users.noreply.github.com>
2024-10-30 23:54:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/README.md
+++ 프로그래머스/0/120846. 합성수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 합성수 찾기 - 120846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.18 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 23:52:27
+2024년 10월 30일 23:54:07
 
 ### 문제 설명
 
--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,8 +1,12 @@
-function solution(n) { 
-    let cnPlusNum = n >= 91 ? 3 : n >= 77 ? 2 : n >= 49 ? 1 : 0;
-    let cnNum = parseInt(n / 2) - 1;   // 4부터(2만 제외) n까지의 짝수 개수 세기
-    for (let i = 9; i <= n; i+=2) { // 9부터, 홀수 중에서만 체크
-        if (i % 3 == 0 || i % 5 == 0) cnPlusNum += 1; // 3의 배수거나 5의 배수면 합성수로 추가
+function solution(n) {
+    let dp = new Array(n+1).fill(1)
+    for(let i = 2 ; i <= n ; i++){
+        if(dp[i]){
+            for(let j = 2 ; i*j <= n ; j++){
+                dp[i*j] = 0
+            }
+        }
     }
-    return n <= 3 ? 0 : n < 9 ? cnNum : cnNum + cnPlusNum;
+
+    return dp.filter(el => el === 0).length
 }
