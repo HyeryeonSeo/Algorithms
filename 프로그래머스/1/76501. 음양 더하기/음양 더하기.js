e20720f3eca4a9f246c273451883064cc9d02080
Ryeon Seo <121484282+HyeryeonSeo@users.noreply.github.com>
2024-12-01 17:07:14 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,8 @@
 function solution(absolutes, signs) {
-    let answerSum = 0;
-     for (let i = 0; i < absolutes.length; i++) {  
-        answerSum += signs[i] == true ? absolutes[i] : absolutes[i] * -1
-     }
-    return answerSum;
+    // for 반복문 - 이중 할 필요없음. 인덱스 i 로 같이 공유하면 되니까
+    let sum = 0;
+    for (let i = 0; i < absolutes.length; i++) {
+        sum += signs[i] === true ? absolutes[i] : absolutes[i]*(-1)
+    }
+     return sum;
 }
